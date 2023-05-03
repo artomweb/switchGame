64aea9412ce5a90128e82b45dcf095d5d59ba79d
artom web <artomweb@gmail.com>
2023-05-03 18:50:45 +0100

    flashing piece

--- js/piece.js
+++ js/piece.js
@@ -1,26 +1,57 @@
 class Piece {
-  constructor(x, y, topColour, bottomColour, shelfPiece) {
-    this.pos = new p5.Vector(x, y);
-    this.originalPos = new p5.Vector(x, y);
-    this.topColour = topColour;
-    this.bottomColour = bottomColour;
-    this.isShelfPiece = shelfPiece;
-    this.r = 120;
-  }
-
-  drawPiece() {
-    push();
-    fill("white");
-    circle(this.pos.x, this.pos.y, this.r);
-    if (this.isShelfPiece) {
-      fill(this.topColour);
-      arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, 3 * HALF_PI, HALF_PI);
-      fill(this.bottomColour);
-      arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, -3 * HALF_PI, -HALF_PI);
-    } else {
-      fill(this.topColour);
-      circle(this.pos.x, this.pos.y, this.r - this.r / 5);
+    constructor(x, y, topColour, bottomColour, shelfPiece, justFlipped) {
+        this.pos = new p5.Vector(x, y);
+        this.originalPos = new p5.Vector(x, y);
+        this.topColour = topColour;
+        this.bottomColour = bottomColour;
+        this.isShelfPiece = shelfPiece;
+        this.r = 120;
+        this.justFlipped = justFlipped;
     }
-    pop();
-  }
+
+    drawPiece(flashItt) {
+        push();
+        fill("white");
+        circle(this.pos.x, this.pos.y, this.r);
+        if (this.isShelfPiece) {
+            fill(this.topColour);
+            arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, 3 * HALF_PI, HALF_PI);
+            fill(this.bottomColour);
+            arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, -3 * HALF_PI, -HALF_PI);
+        } else {
+            if (this.justFlipped) {
+                let from, to, t;
+                if (flashItt < 25) {
+                    t = map(flashItt, 0, 25, 0, 1);
+                    from = color(this.topColour);
+
+                    to = color("white");
+                } else if (flashItt < 50) {
+                    t = map(flashItt, 25, 50, 0, 1);
+
+                    from = color("white");
+
+                    to = color(this.bottomColour);
+                } else if (flashItt < 75) {
+                    t = map(flashItt, 50, 75, 0, 1);
+                    from = color(this.bottomColour);
+                    to = color("white");
+                } else if (flashItt < 100) {
+                    t = map(flashItt, 75, 100, 0, 1);
+                    from = color("white");
+
+                    to = color(this.topColour);
+                }
+
+                let c = lerpColor(from, to, t);
+                console.log(from.levels, to.levels, flashItt, t, c.levels);
+                fill(c);
+            } else {
+                fill(this.topColour);
+            }
+
+            circle(this.pos.x, this.pos.y, this.r - this.r / 5);
+        }
+        pop();
+    }
 }
--- js/serverConnection.js
+++ js/serverConnection.js
@@ -1,4 +1,5 @@
 var socket = io("http://localhost:3000/filler");
+// var socket = io("https://rppi.artomweb.com/filler");
 let gameID;
 
 let requestAlert;
@@ -8,139 +9,137 @@ let serverData;
 
 let allServerData;
 
-document.getElementById("usernameInput").addEventListener("keyup", function (event) {
-  event.preventDefault();
-  if (event.code === "Enter") {
-    document.getElementById("submitButton").click();
-  }
+document.getElementById("usernameInput").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.code === "Enter") {
+        document.getElementById("submitButton").click();
+    }
 });
 
-socket.on("new user list", function (msg) {
-  updateUsersOnline(msg);
+socket.on("new user list", function(msg) {
+    updateUsersOnline(msg);
 });
 
 socket.on("connected client", (data) => {
-  console.log(data.msg);
-  let sketch = document.getElementById("sketch");
-  sketch.style["touch-action"] = "none";
-  if (data.error === undefined) {
-    createMessage("Connection Successful", "", "alert-success");
-    // createMessage("Connection Status", "Connection Successful", "alert-primary", (timed = false));
-  }
+    console.log(data.msg);
+    let sketch = document.getElementById("sketch");
+    sketch.style["touch-action"] = "none";
+    if (data.error === undefined) {
+        createMessage("Connection Successful", "", "alert-success");
+        // createMessage("Connection Status", "Connection Successful", "alert-primary", (timed = false));
+    }
 });
 
 function updateUsersOnline(msg) {
-  console.log(msg);
+    console.log(msg);
 
-  let users = msg.userNameList;
-  let listDiv = document.getElementById("listOfUsers");
+    let users = msg.userNameList;
+    let listDiv = document.getElementById("listOfUsers");
 
-  let myID = msg.yourID;
+    let myID = msg.yourID;
 
-  if (users.length === 0) {
-    listDiv.innerHTML = "no users online";
-    return;
-  }
-
-  let listHTML = [];
-
-  for (let user of users) {
-    let thisUsername = user.userName;
-    if (user.id == myID) {
-      listHTML.push("<div> - " + thisUsername + " (YOU)</div>");
-    } else {
-      if (myID) {
-        listHTML.push("<div> - " + thisUsername + "   <button onclick='requestConnection({id:\"" + user.id + '", userName:"' + thisUsername + "\"})'>Request</button></div>");
-      } else {
-        listHTML.push("<div> - " + thisUsername + "   </div>");
-      }
+    if (users.length === 0) {
+        listDiv.innerHTML = "no users online";
+        return;
     }
-  }
-  listDiv.innerHTML = listHTML.join("");
+
+    let listHTML = [];
+
+    for (let user of users) {
+        let thisUsername = user.userName;
+        if (user.id == myID) {
+            listHTML.push("<div> - " + thisUsername + " (YOU)</div>");
+        } else {
+            if (myID) {
+                listHTML.push("<div> - " + thisUsername + "   <button onclick='requestConnection({id:\"" + user.id + '", userName:"' + thisUsername + "\"})'>Request</button></div>");
+            } else {
+                listHTML.push("<div> - " + thisUsername + "   </div>");
+            }
+        }
+    }
+    listDiv.innerHTML = listHTML.join("");
 }
 
 function requestConnection(user) {
-  console.log("REQUEST SEND TO ", user);
-  createMessage("Request Sent to ", user.userName, "alert-info");
-  socket.emit("request connection", { id: user.id });
+    console.log("REQUEST SEND TO ", user);
+    createMessage("Request Sent to ", user.userName, "alert-info");
+    socket.emit("request connection", { id: user.id });
 }
 
 socket.on("request incoming", (data) => {
-  console.log("Incoming request from: ", data.userName);
-  requestAlert = new bootstrap.Alert(createMessage("REQUEST INCOMING", "From " + data.userName, "alert-primary", 20000, "acceptRequest('" + data.userID + "')"));
+    console.log("Incoming request from: ", data.userName);
+    requestAlert = new bootstrap.Alert(createMessage("REQUEST INCOMING", "From " + data.userName, "alert-primary", 20000, "acceptRequest('" + data.userID + "')"));
 });
 
 function acceptRequest(id) {
-  requestAlert.close();
-  socket.emit("accept request", { id });
+    requestAlert.close();
+    socket.emit("accept request", { id });
 }
 
 function submitUsername() {
-  let usernameField = document.getElementById("usernameInput");
-  console.log(usernameField.value);
-  if (usernameField.value) {
-    socket.emit("username input", { name: usernameField.value });
-  }
+    let usernameField = document.getElementById("usernameInput");
+    console.log(usernameField.value);
+    if (usernameField.value) {
+        socket.emit("username input", { name: usernameField.value });
+    }
 }
 
 function updateUsersList() {
-  socket.emit("get username list");
+    socket.emit("get username list");
 }
 
 socket.on("game update", (data) => {
-  console.log(data);
+    console.log(data);
 
-  allServerData = data;
+    allServerData = data;
 
-  gameID = data.gameID;
+    gameID = data.gameID;
 
-  console.log(data.state[data.yourID]);
+    console.log(data.state[data.yourID]);
 
-  createShelfPieces(data.state[data.yourID]);
+    createGamePieces(data.state);
 
-  createGamePieces(data.state.board);
+    let gameStatus = document.getElementById("gameStatus");
 
-  let gameStatus = document.getElementById("gameStatus");
-
-  if (data.nextPlayer.userID == data.yourID) {
-    gameStatus.innerHTML = "IT's YOUR TURN";
-  } else {
-    gameStatus.innerHTML = "IT's " + data.nextPlayer.userName + "'s TURN";
-  }
+    if (data.nextPlayer.userID == data.yourID) {
+        gameStatus.innerHTML = "IT's YOUR TURN";
+    } else {
+        gameStatus.innerHTML = "IT's " + data.nextPlayer.userName + "'s TURN";
+    }
 });
 
 function leaveGame() {
-  console.log("LEAVE GAME");
-  gameJoinedMessage.close();
+    console.log("LEAVE GAME");
+    gameJoinedMessage.close();
 }
 
 function serverMove(method, move) {
-  console.log("move", move);
-  socket.emit("move", { gameID, method, move });
+    console.log("move", move);
+    socket.emit("move", { gameID, method, move });
 }
 
 socket.on("error", (data) => {
-  createMessage("ERROR", data.msg, "alert-danger");
+    createMessage("ERROR", data.msg, "alert-danger");
 });
 
-socket.on("connect", function () {
-  let gameStatus = document.getElementById("gameStatus");
-  gameStatus.innerHTML = "Connected";
+socket.on("connect", function() {
+    let gameStatus = document.getElementById("gameStatus");
+    gameStatus.innerHTML = "Connected";
 });
 
-socket.on("disconnect", function () {
-  let gameStatus = document.getElementById("gameStatus");
-  gameStatus.innerHTML = "No server connection";
+socket.on("disconnect", function() {
+    let gameStatus = document.getElementById("gameStatus");
+    gameStatus.innerHTML = "No server connection";
 });
 
 socket.on("game over", (data) => {
-  console.log("GAME OVER", data);
-  allServerData = data;
+    console.log("GAME OVER", data);
+    allServerData = data;
 
-  createShelfPieces(data.state[data.yourID]);
-  createGamePieces(data.state.board);
+    createShelfPieces(data.state[data.yourID]);
+    createGamePieces(data.state.board);
 
-  let gameStatus = document.getElementById("gameStatus");
-  gameStatus.innerHTML = data.msg;
-  createMessage("GAME OVER", data.msg, "alert-info", 15000);
+    let gameStatus = document.getElementById("gameStatus");
+    gameStatus.innerHTML = data.msg;
+    createMessage("GAME OVER", data.msg, "alert-info", 15000);
 });
