a3cddcb58488dede3bce1c87d5dc0c5049f5f252
artom web <artomweb@gmail.com>
2023-05-11 18:36:14 +0100

    update for new ai

--- js/index.js
+++ js/index.js
@@ -13,151 +13,131 @@ let dragDistance = 5;
 let flashItt = 0;
 
 function createGamePieces(pieces) {
-    let thesePieces = [
-        [],
-        [],
-        [],
-        [],
-        []
-    ];
-    for (let i = 0; i < pieces.length; i++) {
-        for (let j = 0; j < pieces[i].length; j++) {
-            if (pieces[i][j] === null) continue;
-            console.log(i, j);
-            let thisPiece;
-            if (i === 4) {
-                thisPiece = new Piece(pieces[i][j].x, pieces[i][j].y, pieces[i][j].topColour, pieces[i][j].bottomColour, false, pieces[i][j].justFlipped);
-            } else {
-                let x = i * boxWidth + boxWidth / 2;
-                let y = j * boxHeight + boxHeight / 2;
-                thisPiece = new Piece(x, y, pieces[i][j].topColour, pieces[i][j].bottomColour, false, pieces[i][j].justFlipped);
-            }
-            thesePieces[i][j] = thisPiece;
-        }
+  let thesePieces = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ];
+  for (let i = 0; i < pieces.length; i++) {
+    for (let j = 0; j < pieces[i].length; j++) {
+      if (pieces[i][j] === null) continue;
+      let thisPiece = new Piece(pieces[i][j].x, pieces[i][j].y, pieces[i][j].topColour, pieces[i][j].bottomColour, pieces[i][j].justFlipped, pieces[i][j].hasFlipped);
+      console.log(thisPiece, i, j);
+      thesePieces[i][j] = thisPiece;
     }
-    boardPieces = thesePieces;
-
-    board.shelfColour = allServerData.yourColour;
+  }
+  boardPieces = thesePieces;
+  board.shelfColour = allServerData.yourColour;
 }
 
 function setup() {
-    canvas = createCanvas(800, 1000);
-    canvas.parent("sketch");
+  canvas = createCanvas(800, 1000);
+  canvas.parent("sketch");
 
-    board = new Board();
+  board = new Board();
 
-    boxWidth = width / 4;
-    boxHeight = board.boardHeight / 4;
+  boxWidth = board.boardWidth / 4;
+  boxHeight = board.boardHeight / 4;
 }
 
 function draw() {
-    background("lightpink");
+  background("lightpink");
 
-    board.drawBoard();
+  board.drawBoard();
 
-    for (let i = 0; i < boardPieces.length; i++) {
-        for (let j = 0; j < boardPieces[i].length; j++) {
-            if (boardPieces[i][j] === undefined) continue;
-            boardPieces[i][j].drawPiece(flashItt);
-        }
+  for (let i = 0; i < boardPieces.length; i++) {
+    for (let j = 0; j < boardPieces[i].length; j++) {
+      if (boardPieces[i][j] === null) continue;
+      boardPieces[i][j].drawPiece();
     }
+  }
 
-    if (dragging && allServerData.yourID === allServerData.nextPlayer.userID) {
-        let newDragPos = new p5.Vector(mouseX, mouseY);
-        if (newDragPos.dist(dragStart) > dragDistance) {
-            draggingPiece.pos.x = mouseX;
-            draggingPiece.pos.y = mouseY;
-        }
+  if (dragging && allServerData.yourTurn) {
+    let newDragPos = new p5.Vector(mouseX, mouseY);
+    if (newDragPos.dist(dragStart) > dragDistance) {
+      draggingPiece.pos.x = mouseX;
+      draggingPiece.pos.y = mouseY;
     }
-
-    flashItt++;
-    if (flashItt >= 100) {
-        flashItt = 0;
-    }
+  }
 }
 
 function mouseClicked() {
-    if (!allServerData || allServerData.nextPlayer == null || allServerData.yourID !== allServerData.nextPlayer.userID) return;
-    // If the mouse was clicked and it's inside a piece which has not been moved
-    let newDragPos = new p5.Vector(mouseX, mouseY);
-    if (newDragPos.dist(dragStart) > dragDistance) return;
-    for (let i = 0; i < boardPieces.length; i++) {
-        for (let j = 0; j < boardPieces[i].length; j++) {
-            if (boardPieces[i][j] === null || boardPieces[i][j] === undefined) continue;
-            if (boardPieces[i][j].topColour === allServerData.yourColour) continue;
-            if (dist(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y, mouseX, mouseY) < boardPieces[i][j].r / 2) {
-                console.log("clicked on piece, ", boardPieces[i][j]);
-                console.log("");
-                let coords = getBoardCoords(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y);
-                serverMove("flip", { i: coords.i, j: coords.j });
-            }
-        }
+  if (!allServerData || !allServerData.yourTurn) return;
+  // If the mouse was clicked and it's inside a piece which has not been moved
+  let newDragPos = new p5.Vector(mouseX, mouseY);
+  if (newDragPos.dist(dragStart) > dragDistance) return;
+  for (let i = 0; i < boardPieces.length; i++) {
+    for (let j = 0; j < boardPieces[i].length; j++) {
+      if (boardPieces[i][j] === null || boardPieces[i][j] === undefined) continue;
+      if (boardPieces[i][j].topColour === allServerData.yourColour) continue;
+      if (dist(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y, mouseX, mouseY) < boardPieces[i][j].r / 2) {
+        console.log("clicked on piece, ", boardPieces[i][j]);
+        console.log("");
+        let coords = getBoardCoords(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y);
+        serverMove({ i: coords.i, j: coords.j, i2: coords.i, j2: coords.j });
+      }
     }
+  }
 }
 
 function mousePressed() {
-    if (!allServerData || allServerData.nextPlayer == null || allServerData.yourID !== allServerData.nextPlayer.userID) return;
-    dragStart = new p5.Vector(mouseX, mouseY);
+  if (!allServerData || !allServerData.yourTurn) return;
+  dragStart = new p5.Vector(mouseX, mouseY);
 
-    for (let i = 0; i < boardPieces.length; i++) {
-        for (let j = 0; j < boardPieces[i].length; j++) {
-            if (boardPieces[i][j] === null || boardPieces[i][j] === undefined) continue;
-            if (boardPieces[i][j].topColour !== allServerData.yourColour) continue;
-            if (dist(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y, mouseX, mouseY) < boardPieces[i][j].r / 2) {
-                dragging = true;
-                draggingPieceShelfIndex = -1;
-                draggingPiece = boardPieces[i][j];
-                draggingPiece.origin = new p5.Vector(i, j);
-                console.log("started dragging board piece");
-            }
-        }
+  for (let i = 0; i < boardPieces.length; i++) {
+    for (let j = 0; j < boardPieces[i].length; j++) {
+      if (boardPieces[i][j] === null || boardPieces[i][j] === undefined) continue;
+      if (boardPieces[i][j].topColour !== allServerData.yourColour) continue;
+      if (dist(boardPieces[i][j].pos.x, boardPieces[i][j].pos.y, mouseX, mouseY) < boardPieces[i][j].r / 2) {
+        dragging = true;
+        draggingPiece = boardPieces[i][j];
+        draggingPiece.origin = new p5.Vector(i, j);
+        console.log("started dragging board piece");
+      }
     }
+  }
 }
 
 function movePieceBack(piece) {
-    // If the piece came from the shelf then put it back on shelf
-    if (draggingPieceShelfIndex !== -1) {
-        console.log("piece came from shelf");
-        piece.pos = piece.originalPos.copy();
-        piece.boardCoord = 0;
-    }
-    // If the piece came from the board then put it back on board
-    else {
-        console.log("Piece was on board, tried to move it to bad space");
-        let x = draggingPiece.origin.x * boxWidth + boxWidth / 2;
-        let y = draggingPiece.origin.y * boxHeight + boxHeight / 2;
+  // If the piece came from the shelf then put it back on shelf
+  // if (draggingPiece.origin.x === 4) {
+  //     console.log("piece came from shelf");
+  piece.pos = piece.originalPos.copy();
+  // }
+  // // If the piece came from the board then put it back on board
+  // else {
+  //     console.log("Piece was on board, tried to move it to bad space");
+  //     let x = draggingPiece.origin.x * boxWidth + boxWidth / 2;
+  //     let y = draggingPiece.origin.y * boxHeight + boxHeight / 2;
 
-        piece.pos = new p5.Vector(x, y);
-    }
+  //     piece.pos = new p5.Vector(x, y);
+  // }
 }
 
 function mouseReleased() {
-    if (!allServerData || allServerData.nextPlayer == null || allServerData.yourID !== allServerData.nextPlayer.userID) return;
-    let newDragPos = new p5.Vector(mouseX, mouseY);
-    if (dragging && newDragPos.dist(dragStart) > dragDistance) {
-        // If piece was moved to illegal space
-        let insideShelf = circRect(draggingPiece.pos.x, draggingPiece.pos.y, draggingPiece.r / 2, board.shelfPos.x, board.shelfPos.y, board.shelfWidth, board.shelfHeight);
-        let outsideB = outsideBoard(draggingPiece.pos.x, draggingPiece.pos.y, draggingPiece.r / 2);
-        let { i, j } = getBoardCoords(draggingPiece.pos.x, draggingPiece.pos.y);
-        let occupied = isOccupied(i, j, boardPieces);
+  if (!allServerData || !allServerData.yourTurn) return;
+  let newDragPos = new p5.Vector(mouseX, mouseY);
+  if (dragging && newDragPos.dist(dragStart) > dragDistance) {
+    // If piece was moved to illegal space
+    let insideShelf = circRect(draggingPiece.pos.x, draggingPiece.pos.y, draggingPiece.r / 2, board.shelfPos.x, board.shelfPos.y, board.shelfWidth, board.shelfHeight);
+    let outsideB = outsideBoard(draggingPiece.pos.x, draggingPiece.pos.y, draggingPiece.r / 2);
+    let { i, j } = getBoardCoords(draggingPiece.pos.x, draggingPiece.pos.y);
+    let occupied = isOccupied(i, j, boardPieces);
 
-        if (insideShelf || outsideB || occupied) {
-            console.log(insideShelf, outsideB, occupied);
-            movePieceBack(draggingPiece);
-        } else {
-            if (draggingPieceShelfIndex !== -1) {
-                serverMove("add", { idx: draggingPieceShelfIndex, i, j });
-                console.log("dragged shelf piece");
-                shelfPieces.splice(draggingPieceShelfIndex, 1);
-            } else {
-                console.log("moved piece on board", draggingPiece.boardCoord);
-                serverMove("move", { i1: draggingPiece.origin.x, j1: draggingPiece.origin.y, i2, j2 });
-            }
-            // let x = i * boxWidth + boxWidth / 2;
-            // let y = j * boxHeight + boxHeight / 2;
-            // draggingPiece.pos = new p5.Vector(x, y);
-            // draggingPiece.boardCoord = new p5.Vector(i, j);
-        }
+    if (insideShelf || outsideB || occupied) {
+      console.log(insideShelf, outsideB, occupied);
+      movePieceBack(draggingPiece);
+    } else {
+      console.log("moved piece", draggingPiece);
+      serverMove({ i: draggingPiece.origin.x, j: draggingPiece.origin.y, i2: i, j2: j });
+
+      // let x = i * boxWidth + boxWidth / 2;
+      // let y = j * boxHeight + boxHeight / 2;
+      // draggingPiece.pos = new p5.Vector(x, y);
+      // draggingPiece.boardCoord = new p5.Vector(i, j);
     }
-    dragging = false;
+  }
+  dragging = false;
 }
--- js/piece.js
+++ js/piece.js
@@ -1,55 +1,32 @@
 class Piece {
-    constructor(x, y, topColour, bottomColour, shelfPiece, justFlipped) {
+    constructor(x, y, topColour, bottomColour, justFlipped, hasFlipped) {
         this.pos = new p5.Vector(x, y);
         this.originalPos = new p5.Vector(x, y);
         this.topColour = topColour;
         this.bottomColour = bottomColour;
-        this.isShelfPiece = shelfPiece;
         this.r = 120;
         this.justFlipped = justFlipped;
+        this.hasFlipped = hasFlipped;
     }
 
-    drawPiece(flashItt) {
+    drawPiece() {
         push();
         fill("white");
         circle(this.pos.x, this.pos.y, this.r);
-        if (this.isShelfPiece) {
+        let { i, j } = getBoardCoords(this.pos.x, this.pos.y);
+        if ((i === 4 && (j === 0 || j === 1)) || this.justFlipped) {
             fill(this.topColour);
             arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, 3 * HALF_PI, HALF_PI);
             fill(this.bottomColour);
             arc(this.pos.x, this.pos.y, this.r - this.r / 5, this.r - this.r / 5, -3 * HALF_PI, -HALF_PI);
+            push();
+            stroke("black");
+            let lineWeight = 5;
+            strokeWeight(lineWeight);
+            line(this.pos.x, this.pos.y - (this.r / 2 - this.r / 10) + lineWeight, this.pos.x, this.pos.y + this.r - (this.r / 2 + this.r / 10) - lineWeight);
+            pop();
         } else {
-            if (this.justFlipped) {
-                let from, to, t;
-                if (flashItt < 25) {
-                    t = map(flashItt, 0, 25, 0, 1);
-                    from = color(this.topColour);
-
-                    to = color("white");
-                } else if (flashItt < 50) {
-                    t = map(flashItt, 25, 50, 0, 1);
-
-                    from = color("white");
-
-                    to = color(this.bottomColour);
-                } else if (flashItt < 75) {
-                    t = map(flashItt, 50, 75, 0, 1);
-                    from = color(this.bottomColour);
-                    to = color("white");
-                } else if (flashItt < 100) {
-                    t = map(flashItt, 75, 100, 0, 1);
-                    from = color("white");
-
-                    to = color(this.topColour);
-                }
-
-                let c = lerpColor(from, to, t);
-                console.log(from.levels, to.levels, flashItt, t, c.levels);
-                fill(c);
-            } else {
-                fill(this.topColour);
-            }
-
+            fill(this.topColour);
             circle(this.pos.x, this.pos.y, this.r - this.r / 5);
         }
         pop();
